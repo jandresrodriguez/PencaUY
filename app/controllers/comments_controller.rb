08776cce671755d375a166701cf54a7ccd580930
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2014-06-12 14:52:55 -0300

    Destroy not working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  protect_from_forgery with: :null_session
 
   # GET /comments
   # GET /comments.json
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,6 @@
 class PlayersController < ApplicationController
   before_action :set_player, only: [:show, :edit, :update, :destroy]
+  protect_from_forgery with: :null_session
 
   # GET /players
   # GET /players.json
